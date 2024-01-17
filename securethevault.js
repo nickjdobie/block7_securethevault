bae8e4397a9a182224fe4bc17e5b92389626bba0
nicholas dobie <nicholasjdobie@yahoo.com>
2024-01-17 16:22:16 -0500

    lock code error

---
+++ securethevault.js
@@ -0,0 +1,15 @@
+/*assign
+ */
+
+// assigned all 3 numbers to a variable using different arithmetic operation to each of 3 lock combo numbers
+const combo1 = 2 * 5;
+const combo2 = 20 + 20;
+const combo3 = 41 - 2;
+
+// assigned solution variable, openLock as the 3 previous variables added
+const openLock = `${combo1}-${combo2}-${combo3}`;
+openLock = alert(
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination" +
+    " " +
+    openLock
+);
